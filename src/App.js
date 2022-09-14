0055d98d1c3d51f7a1f84b60ad8b4bbbb1eed934
Vijayaprasath <you@example.com>
2022-09-14 11:09:26 +0530

    added diff components.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import categories from './categories';
+import 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className='categories-container'>
+   {
+    categories.map(({title})=>(
+      <div className='category-container'>
+      
+      <div className='category-body-container'>
+        <h1>{title}</h1>
+         <p>Shop Now</p>
+      </div>
+      </div>
+))
+   }
+   </div>
   );
 }
 
